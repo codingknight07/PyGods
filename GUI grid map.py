478af36f9c5ab9d10f5f8d5b2bab81b9fbe3114b
Tushar Garg <gargtushar739@gmail.com>
2020-08-09 19:18:12 +0530

    Second Update

--- GUI grid map.py
+++ GUI grid map.py
@@ -8,6 +8,7 @@ root = Tk()
 root.title('ISRA')
 root.iconbitmap('C:\ISRA\ISRA LOGO.ico')
 root.geometry('430x500')
+root.resizable(height = False, width=False)
 
 #about window
 def _about():
@@ -51,9 +52,22 @@ my_menu.add_cascade(label = 'Help', command = _help)
 
 my_menu.add_cascade(label = 'About', command = _about)
 
-ques_label = Label(root)
-ques_label.place(anchor = CENTER, relx = 0.5, rely = 0.4)
-ques_label.config(text = 'Hello')
+frame = Frame(root)
+
+ #adding assistant text area
+scroll = Scrollbar(frame)
+scroll.pack(side = RIGHT, fill = Y)        
+ass_text = Text(frame, height = 18 , width=50)
+ass_text.place(anchor = CENTER, relx=0.48,rely=0.49)
+ass_text.config(yscrollcommand = scroll.set)
+scroll.config(command = ass_text.yview)
+frame.pack(fill = BOTH, expand = True)
+#Web search entry
+search_lab = Label(root, text = 'Search Web: ', font = ("Times New Roman", 10, "bold"))
+search_lab.place(relx=0.1 , rely = 0.85, anchor = CENTER)
+search_entry = Entry(root)
+search_entry.config(width = 53)
+search_entry.place(relx=0.56,rely=0.85,anchor=CENTER)
 
 #functioning of mic
 on = True
@@ -75,7 +89,7 @@ offmic = PhotoImage(file = 'offmic.png')
 _offmic = offmic.subsample(18,18)
 
 mic_button = Button(root, image = _onmic, command=start)
-mic_button.place(relx=0.5, rely=0.15, anchor = CENTER)
+mic_button.place(relx=0.5, rely=0.1, anchor = CENTER)
 
 
 
